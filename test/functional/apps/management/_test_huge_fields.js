cd760aa251b2beaae3ff6a4050450358a4b8a4c5
wlathrop21 <wlathrop@cs.brooksource.com>
2022-03-01 16:14:58 -0800

    Skipping Large Test

--- test/functional/apps/management/_test_huge_fields.js
+++ test/functional/apps/management/_test_huge_fields.js
@@ -4,13 +4,14 @@ export default function ({ getService, getPageObjects }) {
   const esArchiver = getService('esArchiver');
   const PageObjects = getPageObjects(['common', 'home', 'settings']);
 
-  describe('test large number of fields @skipcloud', function () {
+  describe.skip('test large number of fields @skipcloud', function () {
     const EXPECTED_FIELD_COUNT = '10006';
     before(async function () {
       await esArchiver.loadIfNeeded('large_fields');
       await PageObjects.settings.navigateTo();
       await PageObjects.settings.clickKibanaIndices();
       await PageObjects.settings.createIndexPattern('testhuge', 'date');
+
     });
 
     it('test_huge data should have expected number of fields', async function () {
